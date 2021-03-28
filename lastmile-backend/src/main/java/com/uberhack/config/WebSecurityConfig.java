088485b6fba9746af1417ff418d22de1e529351b
Radhesh Sarma <radheshsarma29@gmail.com>
2021-03-29 01:59:43 +0530

    Update WebSecurityConfig.java

--- lastmile-backend/src/main/java/com/uberhack/config/WebSecurityConfig.java
+++ lastmile-backend/src/main/java/com/uberhack/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate", "/register","/packages","/hello").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
